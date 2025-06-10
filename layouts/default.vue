8ae511e303bd61ef6a0b618b813fe336a5222f0d
dkvir <kvirika137@gmail.com>
2025-06-10 16:43:24 +0400

    👌 IMPROVE: seo

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,11 +18,6 @@
 import { useHomeStore } from "@/store/home";
 const homeStore = useHomeStore();
 
-const route = useRoute();
-const config = useRuntimeConfig();
-
-const siteUrl = config.public.siteUrl;
-
 useSeoMeta({
   title: "Amanita - Fitness Clubnita",
   ogTitle: "Amanita",
@@ -31,40 +26,6 @@ useSeoMeta({
   ogImage: "/images/share.png",
   twitterCard: "summary_large_image",
 });
-
-// useHead({
-//   titleTemplate: "Amanita - Fitness Club",
-//   meta: [
-//     { name: "og:title", content: "AMANITA" },
-//     {
-//       name: "og:description",
-//       content: "Fitness Club",
-//     },
-//     {
-//       name: "og:url",
-//       content: () => siteUrl + route.fullPath,
-//     },
-//     {
-//       name: "og:image",
-//       content: siteUrl + "images/share.png",
-//     },
-//     { name: "og:image:alt", content: "Not a gym. A journey." }, // Optionally, add an alt tag for accessibility
-//     { name: "twitter:card", content: "summary_large_image" },
-//     { name: "twitter:title", content: "Amanita" },
-//     {
-//       name: "twitter:description",
-//       content: "Not a gym. A journey.",
-//     },
-//     {
-//       name: "twitter:image",
-//       content: "/images/share.png",
-//     },
-//     {
-//       name: "description",
-//       content: "Not a gym. A journey.",
-//     },
-//   ],
-// });
 
 onMounted(() => {
   homeStore.changeWebsiteLoaded(true);
